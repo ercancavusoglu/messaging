c0a24907b0a182ca8f589aa3d918ad575cd4a1ee
Ercan Çavuşoğlu <ercan.cavusoglu@enuygun.com>
2025-02-23 23:36:28 +0300

    logger ports added

---
+++ internal/ports/logger.go
@@ -0,0 +1,16 @@
+package ports
+
+// Logger defines the logging operations
+type Logger interface {
+	Debug(args ...interface{})
+	Debugf(format string, args ...interface{})
+	Info(args ...interface{})
+	Infof(format string, args ...interface{})
+	Warn(args ...interface{})
+	Warnf(format string, args ...interface{})
+	Error(args ...interface{})
+	Errorf(format string, args ...interface{})
+	Fatal(args ...interface{})
+	Fatalf(format string, args ...interface{})
+	WithFields(fields map[string]interface{}) Logger
+}
